d79dec3d4c33c6a0912fc9e381f5717ef932e70f
Luis Eduardo <luis@navitdigital.com.br>
2025-10-30 15:22:32 -0300

    feat: vuetify configuration

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,12 +1,21 @@
-import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
+import { createApp } from "vue";
 import { createPinia } from "pinia";
-import "vuetify/styles";
 import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+// @ts-ignore: side-effect import has no type declarations
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
-app.use(createVuetify());
+app.use(
+  createVuetify({
+    components,
+    directives,
+  })
+);
 app.mount("#app");
