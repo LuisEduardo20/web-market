bf61ab0570079e8db4505cea145243169f5dd818
Luis Eduardo <luis@navitdigital.com.br>
2025-10-31 09:52:50 -0300

    feat: web version login page complete

--- apps/frontend/src/views/Login/Login.vue
+++ apps/frontend/src/views/Login/Login.vue
@@ -1,5 +1,14 @@
 <template>
-  <v-container class="bg-red d-flex align-center justify-center">
+  <v-container
+    class="h-100 d-flex flex-column ga-8 align-center page-container justify-center bg-blue-lighten-5"
+  >
+    <div class="d-flex flex-column align-center">
+      <h2 class="text-title-1 text-medium-emphasis">Bem vindo de volta!</h2>
+      <p class="text-body-1 text-medium-emphasis">
+        Por favor, insira suas credenciais para continuar.
+      </p>
+    </div>
+
     <v-sheet
       class="x-auto pa-12 pb-8"
       elevation="8"
@@ -11,22 +20,22 @@
         <v-text-field
           v-model="email"
           :rules="emailRules"
-          label="E-Mail"
+          label="Email"
           prepend-inner-icon="mdi-email"
         ></v-text-field>
 
         <v-text-field
           :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
           :type="visible ? 'text' : 'password'"
-          v-model="lastName"
-          :rules="lastNameRules"
+          v-model="password"
+          :rules="passwordRules"
           label="Senha"
           prepend-inner-icon="mdi-lock-outline"
           @click:append-inner="visible = !visible"
         ></v-text-field>
 
         <div
-          class="text-subtitle-1 text-medium-emphasis mt-4 d-flex align-center justify-space-between"
+          class="text-subtitle-1 text-medium-emphasis mt-4 mb-2 d-flex justify-end"
         >
           <a
             class="text-caption text-decoration-none text-blue"
@@ -42,6 +51,20 @@
           Login
         </v-btn>
       </v-form>
+
+      <div class="pt-4 d-flex align-center justify-center ga-2">
+        <p class="text-subtitle-1 text-medium-emphasis">
+          Não tem uma conta ainda?
+        </p>
+        <a
+          class="text-decoration-none text-blue"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Cadastre-se aqui</a
+        >
+      </div>
     </v-sheet>
   </v-container>
 </template>
@@ -53,13 +76,18 @@ const visible = ref(false);
 const email = ref("");
 const emailRules = [
   (value: string) => {
-    if (/[^0-9]/.test(value)) return true;
-    return "Insira um e-mail corretamente.";
+    if (
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        value
+      )
+    )
+      return true;
+    return "Insira um e-mail válido.";
   },
 ];
 
-const lastName = ref("123");
-const lastNameRules = [
+const password = ref("");
+const passwordRules = [
   (value: string) => {
     if (value?.length >= 6) return true;
     return "A senha deve ter no mínimo 6 dígitos.";
@@ -67,6 +95,8 @@ const lastNameRules = [
 ];
 
 const onSubmit = () => {
-  alert(`E-Mail: ${email.value}\nPassword: ${lastName.value}`);
+  alert(`E-Mail: ${email.value}\nPassword: ${password.value}`);
 };
 </script>
+
+<style scoped lang="scss" src="./styles.scss"></style>
